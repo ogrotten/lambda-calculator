0d671dc1f587a89e8b3334a08db50f4fa9451c66
Darrin Lowery <lambda@monsto.com>
2019-10-08 17:10:37 -0500

    styled the buttons.

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -14,14 +14,14 @@ const Numbers = () => {
 	// STEP 2 - add the imported data to state
 	const [allnum] = useState(numbers);
 	return (
-		<div classname="numbers">
+		<>
 			{/* STEP 3 - Use .map() to iterate over your array data and return a button
 			component matching the name on the provided file. Pass
 			it any props needed by the child component*/}
 			{allnum.map((num, index) => (
 				<NumberButton key={num} num={num} />
 			))}
-		</div>
+		</>
 	);
 };
 
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SpecialButton = props => {
   return (
-    <button className="">
+    <button className="button">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.spec}
     </button>
